386d1e225dbc044fd1577f5929fb23eb20f16f09
MyDemo <gondaliyaaryan099@gmail.com>
2025-10-26 19:56:38 +0530

    variable in js

--- 01Basics/01_variable.js
+++ 01Basics/01_variable.js
@@ -1,11 +1,19 @@
 const accountid = 123455;
-let accountName = "Aryan";
-var accountPass = "12345";
-EnrollNo = "240170146024";
+let accountName = "Aryan"; //* use always let variable because it has a block scope.
+// CONST & LET is part of New version ES6
+
+var accountPass = "12345"; //* don't use var variable because it has a function scope.
+// part of old virsion
+
+EnrollNo = "240170146024"; //! it's called global variable do not use this. it would not work in strict mode it will be crash our code.
+
+// accountid = 98348; //! This is not allowed because constant value does not change !
 
 console.log(accountid);
 console.log(accountName);
 console.log(accountPass);
 
-console.table([accountid,accountName,accountPass,EnrollNo]);
+console.table([accountid,accountName,accountPass,EnrollNo]); //* whenever we want to try print list of variables in table form then used this code.
+ 
 
+//! Note : JavaScript is dynamically typed, meaning — You don’t need to specify the type — it changes automatically based on the value.
