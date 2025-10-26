6669e8ce72d8afc403e022d3e3b1321fe3735910
MyDemo <gondaliyaaryan099@gmail.com>
2025-10-26 19:13:27 +0530

    variable in js

---
+++ 01Basics/01_variable.js
@@ -0,0 +1,11 @@
+const accountid = 123455;
+let accountName = "Aryan";
+var accountPass = "12345";
+EnrollNo = "240170146024";
+
+console.log(accountid);
+console.log(accountName);
+console.log(accountPass);
+
+console.table([accountid,accountName,accountPass,EnrollNo]);
+
