f734d5f42e84d72ea58cf84a5bc0a57383a66091
MyDemo <gondaliyaaryan099@gmail.com>
2025-11-26 23:07:17 +0530

    memory concept

--- 01Basics/test.js
+++ 01Basics/test.js
@@ -1,1 +1,9 @@
 console.log("Aryan_Gondaliya")
+
+/*
+   stack Memory --> in this memory value passed as a copy
+   string,number,boolean,(except objects).
+
+   heap memory --> in this memory value is passed as a reference(address)
+   Object.
+*/ 
